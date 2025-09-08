5ad63b29ca000fdceb3e8185f5cdc65e498441a4
iCyCat <icycat313@gmail.com>
2025-09-08 17:31:44 +0330

    Creating prompt file | update main file | connect channeltracking to ai and make a good format to send message

--- channel_tracking/channeltracking.py
+++ channel_tracking/channeltracking.py
@@ -2,6 +2,7 @@ import json
 import os
 from pyrogram import Client, filters
 import asyncio
+from google import genai
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SETTING_FILE = os.path.join(BASE_DIR, "data-setting.json")
@@ -21,6 +22,8 @@ number = str(data['number'])
 if not number.startswith("+"):
     number = "+" + number
 source_channels: list = data['source_channels']
+if data['ai']['val'] :
+    ai_api = data['ai']['api_token']
 
 acc = Client(
     'tg-channel-tracker',
@@ -29,6 +32,10 @@ acc = Client(
     phone_number=number,
     proxy=dict(hostname=proxy_host, port=proxy_port, scheme="socks5") if data['local_proxy_enabled'] else None
 )
+prompt_file = os.path.join(BASE_DIR,"prompt.txt")
+with open(prompt_file,'r') as f :
+    prompt = f.read()
+gemini = genai.Client(api_key=ai_api)
 
 def run_channel_tracker():
     app = Client(
@@ -41,13 +48,22 @@ def run_channel_tracker():
 
     @app.on_message(filters.channel)
     def get_channel_messages(client: Client, message):
-        # if source_channels and message.chat.id not in source_channels:
-        #     return
+        if source_channels and message.chat.id not in source_channels:
+            return
 
-        print(f"new message from: {message.chat.title}")
+        print(f"New post from [{message.chat.title}]")
+
+        try:
+            response = gemini.models.generate_content(
+                model="gemini-2.5-flash", contents=f"{prompt}\n{message.text}"
+            )
+            print(f"Ai done his work (on {message.chat.title}), end message to telgram ...")
+        except Exception as e:
+            print(f'ther is an error in Ai responsing:\n{e}')
+
         client.send_message(
             chat_id=target_chnl,
-            text=f"new feed from channel {message.chat.title}:\n\n{message.text}"
+            text=f"{response.text}"
         )
 
     print("Channel tracker is running...")
